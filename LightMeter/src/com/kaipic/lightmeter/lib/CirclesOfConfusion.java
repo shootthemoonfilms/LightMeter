cc4f9fe316db8878f2fdb53e1c6a9b000d9f512f
Jeff Buchbinder <jeffreymbuchbinder@gmail.com>
2014-03-24 08:51:04 -0400

    Add more CoC values for more bodies.

--- LightMeter/src/com/kaipic/lightmeter/lib/CirclesOfConfusion.java
+++ LightMeter/src/com/kaipic/lightmeter/lib/CirclesOfConfusion.java
@@ -7,17 +7,20 @@ import java.util.List;
 public class CirclesOfConfusion {
   private static final List<CirclesOfConfusion> values = new ArrayList<CirclesOfConfusion>();
 
-  public final static CirclesOfConfusion FOUR_THIRDS = new CirclesOfConfusion(new Length(0.015f), "4/3");
-  public final static CirclesOfConfusion APS_C = new CirclesOfConfusion(new Length(0.018f), "APS-C");
-  public final static CirclesOfConfusion FULL_FRAME_35MM = new CirclesOfConfusion(new Length(0.03f), "35mm Full-frame");
+  public final static CirclesOfConfusion NIKON_ONE = new CirclesOfConfusion(new Length(0.011f), "Nikon 1 series");
+  public final static CirclesOfConfusion BMCC_M43 = new CirclesOfConfusion(new Length(0.013f), "Blackmagic Cinema Camera MFT");
+  public final static CirclesOfConfusion FOUR_THIRDS = new CirclesOfConfusion(new Length(0.015f), "Micro 4/3 (M43)");
+  public final static CirclesOfConfusion BMPC = new CirclesOfConfusion(new Length(0.016f), "Blackmagic Production Camera 4K");
+  public final static CirclesOfConfusion APS_C = new CirclesOfConfusion(new Length(0.018f), "APS-C Canon/Generic");
+  public final static CirclesOfConfusion APS_C_NIKON = new CirclesOfConfusion(new Length(0.019f), "APS-C Nikon/Pentax/Sony");
+  public final static CirclesOfConfusion APS_H = new CirclesOfConfusion(new Length(0.023f), "APS-H Canon");
+  public final static CirclesOfConfusion FULL_FRAME_35MM = new CirclesOfConfusion(new Length(0.029f), "35mm Full-frame");
   public final static CirclesOfConfusion MF6x45 = new CirclesOfConfusion(new Length(0.047f), "Medium Format 6x4.5");
   public final static CirclesOfConfusion MF6x6 = new CirclesOfConfusion(new Length(0.053f), "Medium Format 6x6");
   public final static CirclesOfConfusion MF6x7 = new CirclesOfConfusion(new Length(0.059f), "Medium Format 6x7");
   public final static CirclesOfConfusion MF6x17 = new CirclesOfConfusion(new Length(0.12f), "Medium Format 6x17");
   public final static CirclesOfConfusion LF4x5 = new CirclesOfConfusion(new Length(0.11f), "Large Format 4x5");
   public final static CirclesOfConfusion LF5x7 = new CirclesOfConfusion(new Length(0.15f), "Large Format 5x7");
-
-
   public final static CirclesOfConfusion LF8x10 = new CirclesOfConfusion(new Length(0.22f), "Large Format 8x10");
 
   private final String stringValue;
